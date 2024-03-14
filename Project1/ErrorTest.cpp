8e73d5be212c937348a7d81569aab98873d23650
다히 <102225999+daheeya@users.noreply.github.com>
2024-03-14 21:38:40 +0900

    오류코드 수정

--- Project1/ErrorTest.cpp
+++ Project1/ErrorTest.cpp
@@ -6,30 +6,33 @@ struct Complex {
 	int image;
 };
 
-void init(Complex *p1) {};
+void init(Complex *p1);
 void init(int rr, int ii, Complex *p2);
 void show(Complex *p3);
 
 void main()
 {
 	cout << "학번: 22124662" << "이름: 한다희";
-	Complex x = nullptr, y = nullptr;
+	Complex x,y;
 	init(&x);
-	init(40, 50, &y);
+	init(40, 50, &x);
 	show(&x);
 	show(&y);
 }
 
+void init(Complex *p1)
+{
+	p1 -> real=2;
+	p1 -> image =5;
+}
+
 void init(int rr, int ii, Complex* p2)
 {
-	{
 		p2->real = rr;
 		p2->image = ii;
-	}
-
 }
 
 void show(Complex *p3)
 {
-	cout << "(" << p3-> real << "+" << p3-> image << "i)" << endl;
+	cout << "(" << p3-> real << "+" << p3-> image << "i)" >> endl;
 }
