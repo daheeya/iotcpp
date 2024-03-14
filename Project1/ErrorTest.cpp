e9f19f4a8d8c6057ae67c01a1637427a4895a8c5
다히 <102225999+daheeya@users.noreply.github.com>
2024-03-14 13:57:17 +0900

    Update ErrorTest.cpp
    CallByReference, CallByValue

--- Project1/ErrorTest.cpp
+++ Project1/ErrorTest.cpp
@@ -13,11 +13,11 @@ void show(Complex *p3);
 void main()
 {
 	cout << "학번: 22124662" << "이름: 한다희";
-	Complex *x = nullptr, *y = nullptr;
-	init(x);
-	init(40, 50, x);
-	show(x);
-	show(y);
+	Complex x = nullptr, y = nullptr;
+	init(&x);
+	init(40, 50, &y);
+	show(&x);
+	show(&y);
 }
 
 void init(int rr, int ii, Complex* p2)
@@ -31,5 +31,5 @@ void init(int rr, int ii, Complex* p2)
 
 void show(Complex *p3)
 {
-	cout << "(" << p3-> real << "+" << p3-> image << "i)" >> endl;
+	cout << "(" << p3-> real << "+" << p3-> image << "i)" << endl;
 }
